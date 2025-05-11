5d1e82cd62ee647d2e8e97d54635aeacde0da218
ufukerentokoglu <ufukerentokoglu@gmail.com>
2025-05-11 18:35:00 +0300

    Oyuncu bot isabet oranı arttıldı

--- core/src/main/java/com/penaltygame/bot/OyuncuBot.java
+++ core/src/main/java/com/penaltygame/bot/OyuncuBot.java
@@ -6,10 +6,20 @@ public class OyuncuBot {
     public float yukseklik;
     public float sutGucu;
 
-    public void sutHesapla(){
+    // Bot oyuncunun şut yönü, yüksekliği ve gücünün rastgele belirlendiği yer.
 
-        sutYonu = (float)((Math.random() * 2) - 1);
-        yukseklik = (float)(0.2 + (Math.random() * 0.6));
-        sutGucu = (float)(0.5 + (Math.random() * 0.5));
+    public void sutHesapla() {
+        double r = Math.random();
+
+        if (r < 0.95) {
+            sutYonu = (float)(80 + Math.random() * 20);
+        }
+        else {
+            sutYonu = Math.random() < 0.5 ? (float)(45 + Math.random() * 30)
+                : (float)(105 + Math.random() * 30);
+        }
+
+        yukseklik = (float)(0.4 + Math.random() * 0.2); // 0.4–0.6
+        sutGucu = (float)(0.65 + Math.random() * 0.2);  // 0.65–0.85
     }
 }
